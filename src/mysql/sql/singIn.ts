6a36a1e94941f6779074ba42c6a57861526e9525
Niiko235 <nicolaseplaza@gmail.com>
2025-10-25 16:30:29 -0500

    delete onboarding_terminado value

--- src/mysql/sql/singIn.ts
+++ src/mysql/sql/singIn.ts
@@ -15,7 +15,6 @@ type User = {
   contrasenia: string
   numero_telefonico: string
   email: string
-  onboarding_terminado: boolean
 }
 
 
@@ -29,7 +28,7 @@ export async function singIn({ correo, contrasenia }: credentialsProps) {
     mysql = await createMySqlClient()
 
     const [response] = await mysql.query<(User & RowDataPacket)[]>(
-      'SELECT id, nombres, apellidos, correo, numero_telefonico, contrasenia, onboarding_terminado FROM estudiantes WHERE correo = ? AND contrasenia = ?',
+      'SELECT id, nombres, apellidos, correo, numero_telefonico, contrasenia FROM estudiantes WHERE correo = ? AND contrasenia = ?',
       [correo, contrasenia]
     )
 
@@ -41,7 +40,6 @@ export async function singIn({ correo, contrasenia }: credentialsProps) {
       id: response[0].id,
       nombres: response[0].nombres,
       email: response[0].correo,
-      onboarding_termiando: response[0].onboarding_terminado,
       apellidos: response[0].apellidos,
       numero_telefonico: response[0].numero_telefonico,
       contrasenia: response[0].contrasenia,
